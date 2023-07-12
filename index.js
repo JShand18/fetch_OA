c28d1caf2b08d6836416dd3df98f8cf8446f39c2
JShand18 <jor.shand@gmail.com>
2023-07-12 01:40:56 -0400

    Refactored index.js to use postman testing

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//******* PACKAGES AND SETUP */
 const express = require('express');
 const bp = require("body-parser");
 const fileUpload = require('express-fileupload');
@@ -5,88 +6,97 @@ const crypto = require('crypto');
 const { type } = require('os');
 
 const app = express();
+const port = 3000;
 
-app.use(fileUpload());
+//app.use(fileUpload());
 app.set('view engine', 'ejs');
 app.use(bp.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(bp.json());
 
-var receipts = new Map();
 
-app.get('/index', function (req, res) {
-    res.render('launch', { receipts: receipts });
-})
+//******* DATA STORES */
+
+// Initilizing map to store recepits and their ids
+var receiptStore = new Map(); // Key: id, Value: Receipt JSON
+
+// Initilizing map to store ids and thier points 
+var pointsStore = new Map(); // Key: id, Value: awarded points
+
+
+//******* ENDPOINT ROUTING */
 
 app.post('/receipts/process', (req, res) => {
-    //Checking to see if form action was a file upload
-    if (req.files) {
-        // retriving file from the request body
-        var file = req.files.file;
-        // validate that file is a JSON file
-        if (validateJSON(file.data)) {
-            var receipt = JSON.parse(file.data);
-            var id = crypto.createHash('md5').update(JSON.stringify(receipt)).digest('hex');
-            //var id = file.md5;
-            // checking to see if the receipt already exists
-            if (receipts.has(id)) {
-                console.log(`Receipt ${id} already being track.`);
-            } else {
-                receipts.set(id, receipt);
-                //res.json({"id" : id});
-            }
-        } else {
-            console.log("Not valid JSON file");
-        }
+    var data = req.body;
+    // if (validateJSON(data)) {
+    var receipt = data;
+    // creating ID based on md5 hashing
+    // duplicate receipts will have the same IDs
+    var id = crypto.createHash('md5').update(JSON.stringify(receipt)).digest('hex');
+    // checking to see if the receipt already exists
+    if (!receiptStore.has(id)) {
+        receiptStore.set(id, receipt);
     }
-    else if (req.body.text) {
-        var text = req.body.text;
-        if (validateJSON(text)) {
-            var receipt = JSON.parse(text);
-            var id = crypto.createHash('md5').update(JSON.stringify(receipt)).digest('hex');
-            if (receipts.has(id)) {
-                console.log(`Receipt ${id} already being track.`);
-            } else {
-                receipts.set(id, receipt);
-                //res.json({"id" : id});
-            }
-        } else {
-            console.log(text);
-            console.log("Not valid JSON formatted text");
-        }
-    }
-    else {
-        console.log("No valid JSON provided");
-    }
-    res.redirect('/index');
+    res.json({ "id": id });
 });
 
+// receipts/id/points endpoint
 app.get('/receipts/:id/points', (req, res) => {
-    var receipt = receipts.get(req.params.id);
-    res.json({ "points": calculateTotalSumPoints(receipt) });
-
+    var id = req.params.id;
+    var receipt = receiptStore.get(id);
+    // check to see if the receipt's points have already been calculated and stored
+    if (pointsStore.has(id)) {
+        res.json({ "points": pointsStore.get(id) });
+    } else {
+        // calculates and saves receipts points
+        var points = calculateTotalSumPoints(receipt);
+        pointsStore.set(id, points);
+        res.json({ "points": points });
+    }
 });
 
 
+//******* CALCULATION HELPER FUNCTIONS */
+
+/** calculateTotalSumPoints
+ *      @params receipt - JSON formatted receipt
+ *      @returns totalPoints – int, total amount of points
+ *      Calculates points of a receipt based on set rules:
+ *          One point for every alphanumeric character in the retailer name.
+ *          50 points if the total is a round dollar amount with no cents.
+ *          25 points if the total is a multiple of 0.25.
+ *          5 points for every two items on the receipt.
+ *          If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
+ *          6 points if the day in the purchase date is odd.
+ *          10 points if the time of purchase is after 2:00pm and before 4:00pm. 
+ */
 function calculateTotalSumPoints(receipt) {
 
     var totalPoints = 0;
     totalPoints += calculateRetailerPoints(receipt['retailer']);
     totalPoints += calculateTotalDollarPoints(receipt['total']);
-    totalPoints += calculatePurchaseTimePoints(receipt['purchaseTime']);
-    totalPoints += calculatePurchaseDatePoints(receipt['purchaseDate']);
     totalPoints += calculateItemsPoints(receipt['items']);
-
-    console.log(totalPoints);
-
+    totalPoints += calculatePurchaseDatePoints(receipt['purchaseDate']);
+    totalPoints += calculatePurchaseTimePoints(receipt['purchaseTime']);
 
     return totalPoints;
 }
 
+/** calculateRetailerPoints
+ *      @params retailer – string, name of retailer store
+ *      @returns points – int, amount of points
+ *      Calculates one point for every alphanumeric character in the retailer name.
+ */
 function calculateRetailerPoints(retailer) {
     var regex = /[a-zA-Z0-9]/g;
     return retailer.match(regex).length;
 }
 
+/** calculateTotalDollarPoints
+ *      @params total – string, total amount of item prices
+ *      @returns points – int, amount of points
+ *      Calculates 50 points if the total is a round dollar amount with no cents.
+ *      Calculates 25 points if the total is a multiple of 0.25.
+ */
 function calculateTotalDollarPoints(total) {
     var points = 0;
     var amount = parseFloat(total);
@@ -99,19 +109,13 @@ function calculateTotalDollarPoints(total) {
     return points;
 }
 
-function calculatePurchaseTimePoints(time) {
-    var hour = parseInt(time.split(":")[0]);
-    if (hour >= 14 && hour <= 16) {
-        return 10;
-    } else { return 0; }
-}
-
-function calculatePurchaseDatePoints(date) {
-    if (parseInt(date.substr(-1)) % 2 == 1) {
-        return 6;
-    } else { return 0; }
-}
-
+/** calculatePurchaseDatePoints
+ *      @params items – object, list of item objects with description and price of item
+ *      @returns points – int, amount of points
+ *      Calculates 5 points for every two items on the receipt.
+ *      If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
+ *          6 points if the day in the purchase date is odd.
+ */
 function calculateItemsPoints(items) {
     var points = 0;
     // Calculate the points for pairs of items
@@ -126,26 +130,52 @@ function calculateItemsPoints(items) {
     return points;
 }
 
-function validateJSON(data) {
-    try {
-        JSON.parse(data);
-        return true;
-    } catch (err) {
-        return false;
-    }
+/** calculatePurchaseDatePoints
+ *      @params date – string, date of purchase
+ *      @returns points – int, amount of points
+ *      Calculates 6 points if the day in the purchase date is odd.
+ */
+function calculatePurchaseDatePoints(date) {
+    if (parseInt(date.substr(-1)) % 2 == 1) {
+        return 6;
+    } else { return 0; }
 }
 
+/** calculatePurchaseTimePoints
+ *      @params time – string, time of purchase
+ *      @returns points – int, amount of points
+ *      Calculates 10 points if the time of purchase is after 2:00pm and before 4:00pm. 
+ */
+function calculatePurchaseTimePoints(time) {
+    var hour = parseInt(time.split(":")[0]);
+    if (hour >= 14 && hour <= 16) {
+        return 10;
+    } else { return 0; }
+}
 
+//******* VALIDATION HELPER FUNCTIONS */
 
-
+/** validateJSON
+ *      @params data – file or text content
+ *      @returns boolean 
+ *      Determines if given file or text data is JSON formatted
+ */
+// function validateJSON(data) {
+//     try {
+//         JSON.parse(data);
+//         return true;
+//     } catch (err) {
+//         return false;
+//     }
+// }
 
 app.use('/', (req, res) => {
-    res.redirect('/index')
+    res.send("Receipt Processor");
 });
 
 
 // This starts the web server on port 3000. 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+    console.log(`Listening on port: http://localhost:${port}`);
 });
 
